7a5253c7663ae309164b871a2dfd87eafbecdaf8
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-12 10:44:43 +0530

    Add files via upload

---
+++ Lecture5part1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    //declare
+    int z[5];
+    cout<<"ENter five integer values";
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<"The integer in reverse order are: "<<e<<d<<c<<b<<a<<endl;
+    
+    return 0;
+}
